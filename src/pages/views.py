586e3867b258cf6ca955ef46045dd741f783b917
crueldad <ian.crueldad@cgu.edu>
2019-10-07 18:27:14 -0700

    Changed views, url pages, edited and added chooseplan, register, and login page

--- src/pages/views.py
+++ src/pages/views.py
@@ -8,3 +8,7 @@ def about(request):
     return render(request, 'pages/about.html')
 def chooseplan(request):
     return render(request, 'pages/chooseplan.html')
+def register(request):
+    return render(request, 'pages/register.html')
+def login(request):
+    return render(request, 'pages/login.html')
